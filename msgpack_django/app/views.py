549d3f4ab5326f05631319d94445d8e27c9c17e6
vbkhanh212 <vbkhanh@emotiv.com>
2025-11-26 09:21:36 +0700

    Feature: Added new app URLs and requirements. Cleanup: Stopped tracking compiled Python files (.pyc)

--- msgpack_django/app/views.py
+++ msgpack_django/app/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from rest_framework_msgpack.parsers import MessagePackParser
+from rest_framework_msgpack.renderers import MessagePackRenderer
 
-# Create your views here.
+class DataView(APIView):
+    """
+    This endpoint accepts data and echoes a response.
+    It supports both JSON and MessagePack based on headers.
+    """
+    parser_classes = [MessagePackParser, JSONParser] 
+    renderer_classes = [MessagePackRenderer, JSONRenderer]
+
+    def post(self, request):
+        # request.data will automatically be parsed by DRF
+        # It could be a dict from JSON or a dict from MsgPack binary data.
+        
+        # Accessing data is exactly the same as always:
+        name = request.data.get('name', 'Guest')
+        age = request.data.get('age', 0)
+        
+        response_data = {
+            'message': f"Hello, {name}! We received your age as {age}.",
+            'data_format': request.accepted_renderer.media_type
+        }
+
+        # DRF's Response object will be serialized by the chosen renderer
+        return Response(response_data, status=status.HTTP_200_OK)
+    
